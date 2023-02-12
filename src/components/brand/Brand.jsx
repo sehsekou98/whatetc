5f804f8eca0987d11f34134363cf1851dd1fd15b
Sekou S. Seh <sehsekou98@gmail.comh>
2023-02-12 02:18:52 +0100

    brand

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
-
+import { google, atlassian, dropbox,shopify, slack } from './import'
+import './Brand.css'
 const Brand = () => {
   return (
-    <div>Brand</div>
+    <div className='gpt3__brand section__padding'>
+      <div>
+        <img src={google} />
+      </div>
+      <div>
+        <img src={slack} />
+      </div>
+      <div>
+        <img src={atlassian} />
+      </div>
+      <div>
+        <img src={dropbox} />
+      </div>
+      <div>
+        <img src={shopify} />
+      </div>
+      </div>
   )
 }
 
